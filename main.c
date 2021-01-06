04811f3e3a91416ca5f302003d601f34ef4b5a64
gabrielvictorcf <zerozen8@hotmail.com>
2021-01-05 23:32:47 -0300

    add KnapsackPopulation struct and tidy code

--- main.c
+++ main.c
@@ -11,6 +11,12 @@
 #define AUGMENTED_MUTATION_RATE 70
 #define DIMINISHED_MUTATION_RATE 2
 
+typedef struct KNAPSACK_POPULATION_{
+	bool** population;
+	int bestFit;
+	int size;
+	int mutationRate;
+}KnapsackPopulation;
 
 // Returns index of inidividual with best fitness on fitnesses
 int maxFitness(long long* fitnesses,int fitnessQty){
@@ -48,10 +54,10 @@ void mutate(bool* cromossome,int chromSize, int mutationRate){
 }
 
 // Returns array with fitness of every individual of population
-long long* evaluatePopulation(KnapsackProblem* kProblem,bool** population,int popSize){
-	long long* popFitnesses = malloc(popSize * sizeof(long long));
-	for(int i = 0;i < popSize;i++){
-		popFitnesses[i] = evaluate_solution(*kProblem,population[i]);
+long long* evaluatePopulation(KnapsackProblem* kProblem,KnapsackPopulation* kPop){
+	long long* popFitnesses = malloc(kPop->size * sizeof(long long));
+	for(int i = 0;i < kPop->size;i++){
+		popFitnesses[i] = evaluate_solution(*kProblem,kPop->population[i]);
 	}
 
 	return popFitnesses;
@@ -66,51 +72,52 @@ void report_to_file(long long* popFitnesses, int popSize, FILE* file){
 
 // prevBest é só pra printar, em funcionalidade não muda nada aliás, a função
 // pode retornar void tbm, só retorna o bestFit pra poder printar tbm
-long long passGeneration(KnapsackProblem* kProblem,bool** population,int popSize,int* select(),long long prevBest, int mutationRate, FILE* report_file){
-	long long* popFitnesses = evaluatePopulation(kProblem,population,popSize);
+long long passGeneration(KnapsackProblem* kProblem,KnapsackPopulation* kPop,int* select(), FILE* reportFile){
+	long long* popFitnesses = evaluatePopulation(kProblem,kPop);
 
-	int bestFit = maxFitness(popFitnesses,popSize);
+	kPop->bestFit = maxFitness(popFitnesses,kPop->size);
 
-	bool** nextPopulation = malloc(popSize * sizeof(bool*));
+	bool** nextPopulation = malloc(kPop->size * sizeof(bool*));
 	int chromSize = kProblem->n_elements;
-	for(int i = 0;i < popSize;i++){
-		if(i == bestFit){	// Guaranteeing that best fitness isn't lost
+	for(int i = 0;i < kPop->size;i++){
+		if(i == kPop->bestFit){	// Guaranteeing that best fitness isn't lost
 			nextPopulation[i] = malloc(kProblem->n_elements);
-			nextPopulation[i] = memcpy(nextPopulation[i],population[i],kProblem->n_elements);
+			nextPopulation[i] = memcpy(nextPopulation[i],kPop->population[i],kProblem->n_elements);
 			continue;
 		}
 
 		// Stores equivalent index of Father + Mother on the population array
-		int* parents = select(popFitnesses,popSize,bestFit);
+		int* parents = select(popFitnesses,kPop->size,kPop->bestFit);
 
-		bool* Father = population[parents[0]];
-		bool* Mother = population[parents[1]];
+		bool* Father = kPop->population[parents[0]];
+		bool* Mother = kPop->population[parents[1]];
 		bool* child = crossover(Father,Mother,chromSize);
-		mutate(child,chromSize,mutationRate);
+		free(parents);
+
+		mutate(child,chromSize,kPop->mutationRate);
 
 		nextPopulation[i] = child;
-		free(parents);
 	}
 
 	// Replace previous population with new generation. Best fitness is preserved!
-	for(int i = 0;i < popSize;i++){
-		free(population[i]);
-		population[i] = nextPopulation[i];
+	for(int i = 0;i < kPop->size;i++){
+		free(kPop->population[i]);
+		kPop->population[i] = nextPopulation[i];
 	}
 	free(nextPopulation);
 
-	// Essa parte é pra garantir que no último print o bestFit vai ser printado pra 
-	// rodar com pop mt grande é melhor printar na inicialização de bestFit mesmo
-	bestFit = maxFitness(popFitnesses,popSize);
-	if(prevBest < popFitnesses[bestFit]){
-		printf("Best fitness: %lld",popFitnesses[bestFit]);
-	}
+	// // Essa parte é pra garantir que no último print o bestFit vai ser printado pra 
+	// // rodar com pop mt grande é melhor printar na inicialização de bestFit mesmo
+	// bestFit = maxFitness(popFitnesses,kPop->size);
+	// if(prevBest < popFitnesses[bestFit]){
+	// 	printf("Best fitness: %lld",popFitnesses[bestFit]);
+	// }
 
-    report_to_file(popFitnesses, popSize, report_file);
+    report_to_file(popFitnesses, kPop->size, reportFile);
 
-    long long v = popFitnesses[bestFit];
+    long long bestFitness = popFitnesses[kPop->bestFit];
     free(popFitnesses);
-	return v; // so pra printar
+	return bestFitness;
 }
 
 // Só pro print da população inicial
@@ -142,69 +149,81 @@ int main(int argc, char const *argv[]){
 	KnapsackProblem kProblem = generate_problem(n_elements,min_capacity,max_capacity,
 												min_value,max_value,min_weight,max_weight);
 	
-	printf("Insira o tamanho da população: ");
-	int popSize; scanf("%d",&popSize);
+	FILE* reportFile = fopen("report.jsonl","w");
 
-	bool** population = malloc(popSize * sizeof(bool*));
-	for(int i = 0; i < popSize;i++){
-		population[i] = generate_solution(kProblem);
-		printSolution(population[i],kProblem);
+	clock_t start = clock();
+    long long greedy = greedy_solution(kProblem);
+    printf("Greedy solution:  %lld\tAchieved in %lf\n",greedy,(double)(clock() - start) / CLOCKS_PER_SEC);
+
+    fprintf(reportFile,"[%lld]\n",greedy);
+
+	if (kProblem.n_elements * kProblem.capacity < 100000000){
+		start = clock();
+        long long optimal = optimal_solution(kProblem);
+        printf("Optimal solution: %lld\tAchieved in %lf\n",optimal,(double)(clock() - start) / CLOCKS_PER_SEC);
+        fprintf(reportFile,"[%lld]\n",optimal);
+	}
+	else{
+		printf("Optimal solution could not be calculated\n");
 	}
 
-    FILE* report_file = fopen("report.jsonl", "w");
+	//TODO: move KnapsackPopulation allocation into separate function
+	KnapsackPopulation* kPop = malloc(sizeof(KnapsackPopulation));
 
-    long long greedy = greedy_solution(kProblem);
-    printf("Greedy solution:  %lld\n", greedy);
-    fprintf(report_file, "[%lld]\n", greedy);
+	printf("Insira o tamanho da população: ");
+	scanf("%d",&kPop->size);
 
-    if (kProblem.n_elements * kProblem.capacity < 100000000){
-        long long opt = optimal_solution(kProblem);
-        printf("Optimal solution: %lld\n", optimal_solution(kProblem));
-        fprintf(report_file, "[%lld]\n", opt);
-    }else
-        printf("Optimal solution could not be calculated\n");
+	kPop->population = malloc(kPop->size * sizeof(bool*));
+	for(int i = 0; i < kPop->size;i++){
+		kPop->population[i] = generate_solution(kProblem);
+		// printSolution(population[i],kProblem);
+	}
 
 	printf("Insira a quantidade de gerações: ");
 	int genQty; scanf("%d",&genQty);
 
-    int mutationRate = STANDARD_MUTATION_RATE;
+	kPop->mutationRate = STANDARD_MUTATION_RATE;
 
 	long long curBest = -1;
     int genCont = 0;
     int mutationHolder = 0;
     int mutationHolderFlag = 0;
-
+	
+	start = clock();	// Actual evolutionary algorithm starts here
 	for(int i = 0;i < genQty;i++){
-		long long nextBest = passGeneration(&kProblem,population,popSize,selectionRoulette,curBest,mutationRate, report_file);
+		long long nextBest = passGeneration(&kProblem,kPop,selectionRoulette,reportFile);
+		
 		if(nextBest > curBest){
-			printf(" on Gen [%d]\n",i);
+			printf("Best fitness: %lld on Gen [%d]\tRate: %d\n",nextBest,i,kPop->mutationRate);
 			curBest = nextBest;
-		} else if (nextBest == curBest && genCont == MUTATION_PERIOD){
+		}else if (nextBest == curBest && genCont == MUTATION_PERIOD){
 			if(mutationHolderFlag == 0){ //Abaixa a taxa de mutação
-				mutationRate = DIMINISHED_MUTATION_RATE;
+				kPop->mutationRate = DIMINISHED_MUTATION_RATE;
 				mutationHolderFlag = 1;
 				genCont = 0;
-			} else if(mutationHolderFlag == 1){ //Aumenta a taxa de mutação
-				mutationRate = AUGMENTED_MUTATION_RATE;
+			}else if(mutationHolderFlag == 1){ //Aumenta a taxa de mutação
+				kPop->mutationRate = AUGMENTED_MUTATION_RATE;
 				mutationHolderFlag = 2;
 				genCont = 0;
-			} else { //Volta a taxa de mutação para o padrão
-				mutationRate = STANDARD_MUTATION_RATE;
+			}else{ 		//Volta a taxa de mutação para o padrão
+				kPop->mutationRate = STANDARD_MUTATION_RATE;
 				mutationHolderFlag = 0;
 				genCont = 0;
 			}
-		}			
+		}
 	}
 
-	printf("Final pop: \n");
-	for(int i = 0; i < popSize;i++){
-		printf("\t");
-		printSolution(population[i],kProblem);
-        free(population[i]);
-	}
-	
+	printf("Final best: %lld\tAchieved in %lf\n",curBest,(double)(clock() - start) / CLOCKS_PER_SEC);
+
     free(kProblem.values);
     free(kProblem.weights);
-    free(population);
+
+	//TODO: move KnapsackPopulation destruction into separate function
+	for(int i = 0;i < kPop->size;i++){
+		free(kPop->population[i]);
+	}
+	free(kPop->population);
+	free(kPop);
+	
 	return 0;
 }
